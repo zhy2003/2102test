73c2f7b02bd2b91c775f543708ad79b332a3ba15
11111 <aaaaa>
2021-05-25 15:37:18 +0800

    初始化

---
+++ a1.php
@@ -0,0 +1,10 @@
+<?php
+//调用 time()函数 获取unix时间戳
+echo time();
+echo "\n";
+//显示当前文件绝对路径
+echo __FILE__;
+echo "\n";
+//显示当前行号
+echo __LINE__;
+?>
---
+++ a2.php
@@ -0,0 +1,9 @@
+<?php
+$b = false;
+$a = 1;
+if($b){
+    echo "真";
+}else{
+    echo "假";
+}
+?>
